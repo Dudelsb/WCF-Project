a3546a058c6a5660509a2de0756f2eb54b0c0639
Dudelsb <x2dzya@gmail.com>
2018-09-05 12:41:06 +0530

    initial commit

---
+++ ClientApplication/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ClientApplication.HelloWorldServiceReference;
+
+namespace ClientApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HelloWorldServiceClient client = new HelloWorldServiceClient("WSHttpBinding_IHelloWorldService");
+            Name first = new Name()
+            {
+                firstName = "Lakhan",
+                lastName = "Bisht"
+            };
+            Console.WriteLine( client.SayHello(first));
+            Console.ReadLine();
+        }
+    }
+}
---
+++ LearningWCF/HelloWorldService.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LearningWCF
+{
+    [DataContract]
+    public class Name
+    {
+        [DataMember]
+        public string firstName { get; set; }
+        [DataMember]
+        public string lastName { get; set; }
+    }
+
+    [ServiceContract]
+    public interface IHelloWorldService
+    {
+        [OperationContract]
+        string SayHello(Name person);
+    }
+
+    public  class HelloWorldService : IHelloWorldService
+    {
+        public string SayHello(Name person)
+        {
+            return string.Format("Hello {0} {1} ", person.firstName, person.lastName);
+        }
+    }
+}
